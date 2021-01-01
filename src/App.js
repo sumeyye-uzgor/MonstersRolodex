ad6dc62f11f94099a6d3c0eef11e4f9289aecfdc
Sümeyye Üzgör <summeyyeoz@gmail.com>
2021-01-01 23:05:58 +0300

    end of tutorial 38

--- src/App.js
+++ src/App.js
@@ -17,11 +17,15 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }))
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
         <input type="search" placeholder="Search monsters"
           onChange={e => this.setState({ searchField: e.target.value })} />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
