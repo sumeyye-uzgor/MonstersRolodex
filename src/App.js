e2192ae2d043781661730d02516b4d7e86402a8b
Sümeyye Üzgör <summeyyeoz@gmail.com>
2021-01-02 01:37:56 +0300

    end of tutorial 48

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
     )
     return (
       <div className="App">
-        <h1> Monsters Rolodex </h1>
+        <h1 id="main-heading"> Monsters Rolodex </h1>
         <SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
         < CardList monsters={filteredMonsters} />
       </div>
